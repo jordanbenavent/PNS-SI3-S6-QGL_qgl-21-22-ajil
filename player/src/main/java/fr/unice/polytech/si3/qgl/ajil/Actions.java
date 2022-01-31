0137e0467f1f65e34511dc0ed5d3d59d63521997
Alexis ROCHE <alexis.roche1@etu.unice.fr>
2022-01-31 13:22:27 +0100

    enum des actions #3

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/ajil/Actions.java
@@ -0,0 +1,6 @@
+package fr.unice.polytech.si3.qgl.ajil;
+
+//Les differentes actions possibles par un marin
+public enum Actions {
+    MOVING, LIFT_SAIL, LOWER_SAIL, TURN, OAR, USE_WATCH, AIM, FIRE, RELOAD;
+}
