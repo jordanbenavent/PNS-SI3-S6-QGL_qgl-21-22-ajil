df95742cce990849bc201ffd29b11bf20160fe82
Alexis ROCHE <alexis.roche1@etu.unice.fr>
2022-03-01 12:51:30 +0100

    Ajout attributs classe Rotation #36

--- player/src/main/java/fr/unice/polytech/si3/qgl/ajil/strategy/GestionRotation.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/ajil/strategy/GestionRotation.java
@@ -1,13 +1,24 @@
 package fr.unice.polytech.si3.qgl.ajil.strategy;
 
+/*
+Cette classe permet de calculer 2 parametres : l'angle de rotation nécessaire avec le gouvernail et l'angle de la rotation
+qui doit être effectué avec les rameurs
+ */
+
 public class GestionRotation {
 
     private double angle; //L'angle duquel on doit tourner
+    private int nbRameurs; //Le nombre de rameurs
     private boolean gouvernailDisponible;
+    private double angleRotationRameur; //Rotation a faire avec les rameurs
+    private double angleRotationGouvernail; //Rotation a faire avec le gouvernail
 
-    GestionRotation(double angle, boolean gouvernailDisponible){
+
+
+    GestionRotation(double angle, boolean gouvernailDisponible,int nbRameurs){
         this.angle=angle;
         this.gouvernailDisponible=gouvernailDisponible;
+        this.nbRameurs=nbRameurs;
     }
 
 
@@ -16,7 +27,7 @@ public class GestionRotation {
         return Math.abs(angle) > Math.PI / 4;
     }
 
-    void typeRotation(){
+    void calculRotation(){
         if(gouvernailDisponible){
             tournerAvecGouvernail();
         }
@@ -30,24 +41,67 @@ public class GestionRotation {
 
 
         if(valeurAngleSuperieurePiSur4()){
+
             //Retrancher parmi les valeurs d'angles de rotation possibles et ajuster avec le gouvernail
 
         }
 
 
         else{ //On utilise que le gouvernail
-
+            angleRotationRameur=0;
+            angleRotationGouvernail = angle;
         }
 
 
     }
 
-    
-
 
     void tournerSansGouvernail(){
         //Ce qu'on faisait avant
 
+    }
+
+
+
+
+
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public boolean isGouvernailDisponible() {
+        return gouvernailDisponible;
+    }
+
+    public void setGouvernailDisponible(boolean gouvernailDisponible) {
+        this.gouvernailDisponible = gouvernailDisponible;
+    }
+
+    public double getAngleRotationRameur() {
+        return angleRotationRameur;
+    }
+
+    public void setAngleRotationRameur(double angleRotationRameur) {
+        this.angleRotationRameur = angleRotationRameur;
+    }
+
+    public double getAngleRotationGouvernail() {
+        return angleRotationGouvernail;
+    }
+
+    public void setAngleRotationGouvernail(double angleRotationGouvernail) {
+        this.angleRotationGouvernail = angleRotationGouvernail;
+    }
+    public int getNbRameurs() {
+        return nbRameurs;
+    }
+    public void setNbRameurs(int nbRameurs) {
+        this.nbRameurs = nbRameurs;
     }
 
 
