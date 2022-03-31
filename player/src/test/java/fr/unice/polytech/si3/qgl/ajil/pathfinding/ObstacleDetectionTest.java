e73a8feffaa9627efd466343f6eb1d1a67dcba80
Jrrdan <jordan.benavent@etu.unice.fr>
2022-03-31 20:33:08 +0200

    Bug

--- player/src/test/java/fr/unice/polytech/si3/qgl/ajil/pathfinding/ObstacleDetectionTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/ajil/pathfinding/ObstacleDetectionTest.java
@@ -10,18 +10,19 @@ public class ObstacleDetectionTest {
 
     @Test
     void findOriginTest(){
+        ObstacleDetection obstacleDetection = new ObstacleDetection();
         Point positionShip = new Point(55,55);
         Point positionCheckpoint = new Point(100,100);
-        Point point = ObstacleDetection.findOrigin(positionShip, positionCheckpoint);
+        Point point = obstacleDetection.findOrigin(positionShip, positionCheckpoint);
         Assertions.assertEquals(new Point(-ObstacleDetection.margin + positionShip.getX(), -ObstacleDetection.margin + positionShip.getY()), point);
         positionCheckpoint = new Point(100,-100);
-        point = ObstacleDetection.findOrigin(positionShip, positionCheckpoint);
+        point = obstacleDetection.findOrigin(positionShip, positionCheckpoint);
         Assertions.assertEquals(new Point(-ObstacleDetection.margin + positionShip.getX(), -ObstacleDetection.margin + positionCheckpoint.getY()), point);
         positionCheckpoint = new Point(-100,-100);
-        point = ObstacleDetection.findOrigin(positionShip, positionCheckpoint);
+        point = obstacleDetection.findOrigin(positionShip, positionCheckpoint);
         Assertions.assertEquals(new Point(-ObstacleDetection.margin + positionCheckpoint.getX(), -ObstacleDetection.margin + positionCheckpoint.getY()), point);
         positionCheckpoint = new Point(-100,100);
-        point = ObstacleDetection.findOrigin(positionShip, positionCheckpoint);
+        point = obstacleDetection.findOrigin(positionShip, positionCheckpoint);
         Assertions.assertEquals(new Point(-ObstacleDetection.margin - positionCheckpoint.getY(), -ObstacleDetection.margin + positionShip.getY()), point);
     }
 
