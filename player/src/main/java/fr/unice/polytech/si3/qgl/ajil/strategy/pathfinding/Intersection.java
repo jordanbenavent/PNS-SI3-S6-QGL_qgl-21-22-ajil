7d3a86ff6ea7598a877558e48b7a0971d001cfdb
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-04-07 09:09:28 +0200

    unit tests

--- player/src/main/java/fr/unice/polytech/si3/qgl/ajil/strategy/pathfinding/Intersection.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/ajil/strategy/pathfinding/Intersection.java
@@ -4,35 +4,27 @@ import fr.unice.polytech.si3.qgl.ajil.shape.Point;
 
 public class Intersection {
 
-    public static Point SegIntersection(Segment segmentOne, Segment segmentTwo)
-    {
+    public static Point segmentIntersection(Segment s1, Segment s2) {
 
-        double dx1 = segmentOne.getEndX()-segmentOne.getStartX() ;
-        double dy1 = segmentOne.getEndY()-segmentOne.getStartY() ;
-        double dx2 = segmentTwo.getEndX()-segmentTwo.getStartX() ;
-        double dy2 = segmentTwo.getEndY()-segmentTwo.getStartY() ;
-        double dxx = segmentOne.getStartX() - segmentTwo.getStartX() ;
-        double dyy = segmentOne.getStartY() - segmentTwo.getStartY() ;
+        double dx1 = s1.getEndX() - s1.getStartX();
+        double dy1 = s1.getEndY() - s1.getStartY();
+        double dx2 = s2.getEndX() - s2.getStartX();
+        double dy2 = s2.getEndY() - s2.getStartY();
+        double dxx = s1.getStartX() - s2.getStartX();
+        double dyy = s1.getStartY() - s2.getStartY();
         double div, t, s;
 
-        div = dy2*dx1-dx2*dy1;
+        div = dy2 * dx1 - dx2 * dy1;
         if (Math.abs(div) < 1.0e-10)  //better to compare abs(div) with small Eps
             return null;  //collinear case
 
-        t = (dx1*dyy-dy1*dxx) / div;
+        t = (dx1 * dyy - dy1 * dxx) / div;
         if (t < 0 || t > 1.0)
             return null; //intersection outside the first segment
 
-        s = (dx2*dyy-dy2*dxx) / div;
+        s = (dx2 * dyy - dy2 * dxx) / div;
         if (s < 0 || s > 1.0)
             return null;  //intersection outside the second segment
-        return new Point(segmentOne.getStartX() + s * dx1, segmentOne.getStartY() + s * dy1);
+        return new Point(s1.getStartX() + s * dx1, s1.getStartY() + s * dy1);
     }
-
-    public static void main(String[] args) {
-        if (Intersection.SegIntersection(new Segment(-1.14,3.39,-1.14,4.39), new Segment(0.48,1.15,4,2.35) ) != null){
-            System.out.println("Intersection");
-        }else System.out.println("Aucune Intersection");
-    }
-
 }
