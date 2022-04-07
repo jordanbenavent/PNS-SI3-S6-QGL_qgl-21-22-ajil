6e3e94cf445dfc6213c33bf3944bd1872ff575c8
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-04-07 09:26:07 +0200

    unit tests Segment

--- player/src/main/java/fr/unice/polytech/si3/qgl/ajil/maths/Segment.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/ajil/maths/Segment.java
@@ -2,15 +2,7 @@ package fr.unice.polytech.si3.qgl.ajil.maths;
 
 import fr.unice.polytech.si3.qgl.ajil.shape.Point;
 
-/**
- * Classe du package maths représentant un segment
- *
- * @author Alexis Roche
- * @author Louis Hattiger
- * @author Jordan Benavent
- * @author Igor Melnyk
- * @author Tobias Bonifay
- */
+import fr.unice.polytech.si3.qgl.ajil.shape.Point;
 
 public class Segment {
     private double startX;
@@ -25,62 +17,34 @@ public class Segment {
         this.endY = b.getY();
     }
 
-    /**
-     * @return la coordonnée X du point de départ du segment
-     */
     public double getStartX() {
         return startX;
     }
 
-    /**
-     * Modifie la coordonnée X du point de départ du segment
-     * @param startX
-     */
     public void setStartX(double startX) {
         this.startX = startX;
     }
 
-    /**
-     * @return la coordonnée Y du point de départ du segment
-     */
     public double getStartY() {
         return startY;
     }
 
-    /**
-     * Modifie la coordonnée Y du point de départ du segment
-     * @param startY
-     */
     public void setStartY(double startY) {
         this.startY = startY;
     }
 
-    /**
-     * @return la coordonnée X du point d'arrivée du segment
-     */
     public double getEndX() {
         return endX;
     }
 
-    /**
-     * Modifie la coordonnée X du point d'arrivée du segment
-     * @param endX
-     */
     public void setEndX(double endX) {
         this.endX = endX;
     }
 
-    /**
-     * @return la coordonnée Y du point d'arrivée du segment
-     */
     public double getEndY() {
         return endY;
     }
 
-    /**
-     * Modifie la coordonnée Y du point d'arrivée du segment
-     * @param endY
-     */
     public void setEndY(double endY) {
         this.endY = endY;
     }
