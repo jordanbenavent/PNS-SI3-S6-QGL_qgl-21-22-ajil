8ac5383b23319bcea3b23ac797ee463768b6d344
Louis Hattiger <74056672+Louishattiger@users.noreply.github.com>
2022-04-16 19:22:00 +0200

    Tests dans NextRound sur la recherche de récifs et de courants dans la liste visibleEntities #75

--- player/src/test/java/fr/unice/polytech/si3/qgl/ajil/NextRoundTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/ajil/NextRoundTest.java
@@ -27,6 +27,7 @@ public class NextRoundTest {
                 new ArrayList<>(),
                 new Rectangle("rectangle", 5, 5, 5));
         wind = new Wind(Math.PI/2, 40);
+        visibleEntities = new ArrayList<>();
         nextRound = new NextRound(ship, wind, visibleEntities);
     }
 
@@ -70,4 +71,37 @@ public class NextRoundTest {
         Assertions.assertEquals(stream4.getPosition().getOrientation(), i.next().getPosition().getOrientation());
     }
 
+    @Test
+    void searchReefTest(){
+        Stream stream1 = new Stream("stream", new Position(0, 50, 0), new Circle("circle", 2), 50);
+        Stream stream2 = new Stream("stream", new Position(10, 0, Math.PI), new Circle("circle", 2), 50);
+        Stream stream3 = new Stream("stream", new Position(10, 90, -0.2), new Circle("circle", 2), 165);
+        Reef reef1 = new Reef("reef", new Position(25, 50, 0), new Circle("circle", 2));
+        Reef reef2 = new Reef("reef", new Position(10, 30, Math.PI), new Circle("circle", 2));
+        visibleEntities.add(stream1);
+        visibleEntities.add(reef1);
+        visibleEntities.add(stream2);
+        visibleEntities.add(stream3);
+        visibleEntities.add(reef2);
+        System.out.println(visibleEntities);
+        nextRound.setVisibleEntities(visibleEntities);
+        Assertions.assertEquals(2, nextRound.searchReef().size());
+    }
+
+    @Test
+    void searchStreamTest(){
+        Stream stream1 = new Stream("stream", new Position(0, 50, 0), new Circle("circle", 2), 50);
+        Stream stream2 = new Stream("stream", new Position(10, 0, Math.PI), new Circle("circle", 2), 50);
+        Stream stream3 = new Stream("stream", new Position(10, 90, -0.2), new Circle("circle", 2), 165);
+        Reef reef1 = new Reef("reef", new Position(25, 50, 0), new Circle("circle", 2));
+        Reef reef2 = new Reef("reef", new Position(10, 30, Math.PI), new Circle("circle", 2));
+        visibleEntities.add(stream1);
+        visibleEntities.add(reef1);
+        visibleEntities.add(stream2);
+        visibleEntities.add(stream3);
+        visibleEntities.add(reef2);
+        System.out.println(visibleEntities);
+        nextRound.setVisibleEntities(visibleEntities);
+        Assertions.assertEquals(3, nextRound.searchStream().size());
+    }
 }
