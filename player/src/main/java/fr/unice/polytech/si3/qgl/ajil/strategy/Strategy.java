322552b43e47f79cdd98ff6cbb890418541fc12e
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2022-04-21 09:48:50 +0200

    Double élargissement #80

--- player/src/main/java/fr/unice/polytech/si3/qgl/ajil/strategy/Strategy.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/ajil/strategy/Strategy.java
@@ -159,7 +159,7 @@ public class Strategy {
     void calculAStar(){
         LOGGER.add("Calcule A Star : Avant A star on a nbcheckpo = "+listeCheckpoints.size());
 
-        AStarDeployment deploy = new AStarDeployment(this.stratData.jeu,150);
+        AStarDeployment deploy = new AStarDeployment(this.stratData.jeu,160);
         List<Checkpoint> fauxCheckpoints = deploy.deployment();
         valideCheckpoint.setFakeCheckpoint(fauxCheckpoints);
         LOGGER.add("On vient de set nb FauxCheck : "+fauxCheckpoints.size());
