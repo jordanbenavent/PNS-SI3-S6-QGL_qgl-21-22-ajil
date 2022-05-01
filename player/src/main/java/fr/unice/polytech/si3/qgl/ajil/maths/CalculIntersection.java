6954c49dd70fdbdb8c2c2a995e47252f73b4fd2d
JordanBenavent <jordan.benavent@etu.unice.fr>
2022-05-01 16:45:18 +0200

    Début review CalculIntersection #88

--- player/src/main/java/fr/unice/polytech/si3/qgl/ajil/maths/CalculIntersection.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/ajil/maths/CalculIntersection.java
@@ -1,6 +1,9 @@
 package fr.unice.polytech.si3.qgl.ajil.maths;
 
+import fr.unice.polytech.si3.qgl.ajil.Checkpoint;
 import fr.unice.polytech.si3.qgl.ajil.Position;
+import fr.unice.polytech.si3.qgl.ajil.Ship;
+import fr.unice.polytech.si3.qgl.ajil.Vector;
 import fr.unice.polytech.si3.qgl.ajil.shape.*;
 
 import java.util.ArrayList;
@@ -36,9 +39,11 @@ public class CalculIntersection {
         return intersectionSegmentsSegments(shape1, position1, shape2, position2);
     }
 
+
+
     public static boolean intersectionCirclePolygone(Circle circle, Position position1, Polygone polygone, Position position2) {
-        List<Point> pointsRectangle = CalculPoints.calculExtremityPoints(polygone, position2);
-        return intersectionCircleSegments(circle, position1, pointsRectangle);
+        List<Point> pointsPolygon = CalculPoints.calculExtremityPoints(polygone, position2);
+        return intersectionCircleSegments(circle, position1, pointsPolygon);
     }
 
     public static boolean intersectionCircleRectangle(Circle circle, Position position1, Rectangle rectangle, Position position2) {
@@ -203,7 +208,291 @@ public class CalculIntersection {
         return pointCircle1.distance(pointCircle2) <= (rs + rc);
     }
 
-    public List<Point> equationSecondDegres(double a, double b, double c) { //ax^2+bx+c==0
-        return new ArrayList<>();
+    // Nouvelle version
+
+    public static List<Point> intersection2(Shape shape1, Position position1, Shape shape2, Position position2) {
+        if (shape1 instanceof Circle circle1 && shape2 instanceof Circle circle2) {
+            return intersectionPointCircleCircle(circle1, position1, circle2, position2);
+        }
+
+        if (shape1 instanceof Circle circle && shape2 instanceof Rectangle rectangle) {
+            return intersectionPointCircleRectangle(circle, position1, rectangle, position2);
+        }
+        if (shape1 instanceof Rectangle rectangle2 && shape2 instanceof Circle circle2) {
+            return intersectionPointCircleRectangle(circle2, position2, rectangle2, position1);
+        }
+        /*
+        if (shape1 instanceof Circle circle && shape2 instanceof Polygone polygone) {
+            return intersectionCirclePolygone(circle, position1, polygone, position2);
+        }
+        if (shape1 instanceof Polygone polygone && shape2 instanceof Circle circle2) {
+            return intersectionCirclePolygone(circle2, position2, polygone, position1);
+        }
+        */
+
+        return new ArrayList<>();//intersectionSegmentsSegments(shape1, position1, shape2, position2);
     }
+
+
+    //ne fonctionne pas à revoir
+    public static List<Point> intersectionPointCircleCircle(Circle circle1, Position position1, Circle circle2, Position position2) {
+        //cercle 1 (x-xc1) ^2 + (y-yc1) = r^2
+        //cercle 2 (x-xc2) ^2 + (y-yc2) = R^2
+        List<Point> result = new ArrayList<>();
+        if(!intersection(circle1, position1, circle2, position2)) return result;
+        double xb = position2.getX();
+        double yb = position2.getY();
+        double xa = position1.getX();
+        double ya = position1.getY();
+        double r = circle2.getRadius();
+        double R = circle1.getRadius();
+        //a = 2xB
+        //b = 2yB
+        //c = x2B + y2B − R2 + r2
+        //l’´equation du second degree : alphax2 − betax + gamma = 0
+        double a = -2*xa + 2*xb;
+        System.out.println( "a = "+a );
+        double b = -2*ya + 2*yb;
+        System.out.println( "b = "+b );
+        double c = (xa*xa) - (xb*xb) + (ya*ya) - (yb*yb) - (R*R) + (r*r);
+        System.out.println( "c = "+c );
+        double alpha = (1+((a*a)/(b*b)));
+        double beta = (-2*xa + (2*a*c)/(b*b) + (2*a*ya)/b);
+        double gamma = (c*c)/(b*b) + ya + (2*c*ya)/b - R*R;
+
+        // delta = (2ac)2 − 4(a2 + b2)(c2 − b2r2)
+        double delta = beta*beta - (4*alpha*gamma);
+        System.out.println(delta);
+        double x1 = (-beta + Math.sqrt(delta)) / 2*alpha;
+        System.out.println(x1);
+        double x2 = ((2*a*c) + Math.sqrt(delta)) / 2*((a*a)+(b*b));
+        double y1;
+        double y2;
+        if(yb != 0) {
+            y1 = (c-(a*x1))/b;
+            y2 = (c-(a*x2))/b;
+        } else {
+            y1 = b/2 + Math.sqrt((R*R) - ((2*c) - (a*a))/(2*a));
+            y2 = b/2 - Math.sqrt((R*R) - ((2*c) - (a*a))/(2*a));
+        }
+        result.add(new Point(x1, y1));
+        result.add(new Point(x2, y2));
+        return result;
+    }
+
+    /**
+     *
+     * @param circle
+     * @param positionCircle
+     * @param point1
+     * @param point2
+     * @return la liste des points de l'intersection entre le cercle et la droite passant par les deux points
+     */
+    public static List<Point> intersectionPointDroiteCircle(Circle circle, Position positionCircle, Point point1, Point point2){
+        List<Point> result = new ArrayList<>();
+        double r = circle.getRadius();
+        double xc = positionCircle.getX();
+        double yc = positionCircle.getY();
+        double xb1 = point1.getX();
+        double yb1 = point1.getY();
+        double xb2 = point2.getX();
+        double yb2 = point2.getY();
+        if (xb1 == xb2) {
+            System.out.println("vertical");
+            return intersectionPointDroiteVerticaleCircle(point1, point2, circle, positionCircle);
+        }
+        double a = (yb2 - yb1) / (xb2 - xb1);
+        double b = (yb1 - a * xb1);
+
+        //Après simplification on obtient une équation du deuxième degré et on obtient donc un delta.
+        //Equation : alpha x^2 + beta x + c = 0
+        double beta = (-2 * xc) + (2 * a * b) -  (2 * a * yc);
+        double alpha = (a * a + 1);
+        double c = (xc * xc) + (b - yc) * (b - yc) - (r * r);
+        double delta = beta * beta - 4 * alpha * c;
+        double x1;
+        double y1;
+        double x2;
+        double y2;
+        if (delta >= 0) {
+            x1 = (-beta - Math.sqrt(delta)) / (2 * alpha);
+            y1 = a * x1 + b;
+            x2 = (-beta + Math.sqrt(delta)) / (2 * alpha);
+            y2 = a * x2 + b;
+            result.add(new Point(x1,y1));
+            result.add(new Point(x2,y2));
+        }
+        return result;
+
+    }
+
+    /**
+     *
+     * @param point1
+     * @param point2
+     * @param cercle
+     * @param positionCercle
+     * @return la liste des points de l'intersection en une droite verticale et le cercle
+     */
+    private static List<Point> intersectionPointDroiteVerticaleCircle(Point point1, Point point2, Circle cercle, Position positionCercle) {
+        //Dans ce cas la droite du bateau est de la forme x=a;
+        List<Point> result = new ArrayList<>();
+        double a = point1.getX();
+        double xc = positionCercle.getX();
+        double yc = positionCercle.getY();
+        double r = cercle.getRadius();
+        double b = -2 * yc;
+        //On obtient une équation du deuxième degré et on obtient ce delta
+        double delta = b * b - 4 * (a * a + xc * xc - 2 * a * xc + yc * yc - r * r);
+        double y1;
+        double y2;
+        if (delta < 0) {
+            return result;
+        }
+        y1 = (-b - Math.sqrt(delta)) / 2;
+        y2 = (-b + Math.sqrt(delta)) / 2;
+        result.add(new Point(point1.getX(), y1));
+        result.add(new Point(point1.getX(), y2));
+        return result;
+    }
+
+    /**
+     *
+     * @param circle
+     * @param positionCircle
+     * @param point1
+     * @param point2
+     * @return la liste des points entre le circle et le segment
+     */
+    public static List<Point> intersectionCircleSegment(Circle circle, Position positionCircle, Point point1, Point point2){
+        List<Point> intersectionDroite = intersectionPointDroiteCircle(circle, positionCircle, point1, point2);
+        List<Point> result = new ArrayList<>();
+        double x1 = point1.getX();
+        double y1 = point1.getY();
+        double x2 = point2.getX();
+        double y2 = point2.getY();
+        double x;
+        double y;
+        for(Point point : intersectionDroite){
+            x=point.getX();
+            y=point.getY();
+            if( ((x1 <= x && x <= x2) || (x1 >= x && x >= x2)) && ((y1 <= y && y <= y2) || (y1 >= y && y >= y2))){
+                result.add(point);
+            }
+        }
+        return result;
+    }
+
+    public static List<Point> intersectionPointCircleRectangle(Circle circle, Position position1, Rectangle rectangle, Position position2) {
+        List<Point> result = new ArrayList<>();
+        List<Point> pointsRectangle = CalculPoints.calculExtremityPoints(rectangle, position2);
+        System.out.println(pointsRectangle);
+        int size = pointsRectangle.size();
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                result.addAll(intersectionCircleSegment(circle, position1, pointsRectangle.get(i), pointsRectangle.get(j)));
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * Méthode calculant les points d'intersection entre la droite de la trajectoire du bateau et le checkpoint (un cercle)
+     *
+     * @param ship
+     * @param checkpoint
+     * @return les points d'intersection
+     */
+    public List<Point> intersection(Ship ship, Vector v_ship, Checkpoint checkpoint) {
+        // (1) équation cercle: (x-checkpoint.x)^2 + (y-checkpoint.y)^2 = R^2
+        double r = ((Circle) checkpoint.getShape()).getRadius();
+        double xc = checkpoint.getPosition().getX();
+        double yc = checkpoint.getPosition().getY();
+
+        // (2) équation de la droite du bateau: y = ax+b
+        // Etape 1: On prend d'abord deux points (à partir du vecteur bateau) pour pouvoir calculer la droite
+        double x1 = ship.getPosition().getX();
+        double y1 = ship.getPosition().getY();
+        double x2 = ship.getPosition().getX() + v_ship.getX();
+        double y2 = ship.getPosition().getY() + v_ship.getY();
+        if(Math.abs(x2 - x1) < 0.0001){
+            return intersectionDroiteVerticaleCircle(ship, checkpoint);
+        }
+
+        // Etape 2: on calcule la pente si x1 != x2
+        double a = (y2 - y1)/(x2 - x1);
+        // Etape 3: On remplace dans l'équation a par la pente et x et y par un point pour trouver b
+        double b = y1 - a*x1;
+
+        // Maintenant on remplace (2) dans (1)
+        // (x − xc)² + (a*x + b −yc)² = R²
+        // x² − 2*x*xc + xc² + a²*x² + b² + yc² + 2(a*x*b − a*x*yc − b*yc) − R² = 0
+        // x²(1 + a²) + x(−2*xc + 2*a*b − 2*a*yc) + (xc² + yc² + b²− 2*b*yc − R²) = 0
+        // On a donc du second degré de la forme ax² + bx + c = 0 avec:
+        ArrayList<Point> points_intersection = new ArrayList<>();
+        double A = 1 + a*a;
+        double B = 2 * (-xc + a*b - a*yc);
+        double C = xc*xc + yc*yc + b*b - 2*b*yc - r*r;
+        double delta = B*B - 4*A*C;
+
+        if (delta > 0) {
+            double x = (-B - Math.sqrt(delta)) / (2*A);
+            double y = a*x + b;
+            points_intersection.add(new Point(x, y));
+
+            x = (-B + Math.sqrt(delta)) / (2*A);
+            y = a*x + b;
+            points_intersection.add(new Point(x, y));
+        }
+        else if (delta == 0) {
+            double x = -B / (2*A);
+            double y = a*x + b;
+            points_intersection.add(new Point(x, y));
+        }
+
+        return points_intersection;
+    }
+
+    /**
+     * Lors d'une droite verticale, l'équation de droite change
+     *
+     * @param ship
+     * @param checkpoint
+     * @return true si le côté du bateau coupe le checkpoint
+     */
+    public List<Point> intersectionDroiteVerticaleCircle(Ship ship, Checkpoint checkpoint) {
+        //Dans ce cas la droite du bateau est de la forme x = a;
+        double a = ship.getPosition().getX();
+        double xc = checkpoint.getPosition().getX();
+        double yc = checkpoint.getPosition().getY();
+        double r = ((Circle) checkpoint.getShape()).getRadius();
+
+        // (a − xc)² + (y − yc)² = R²
+        // a² − 2*a*xc + xc² + y² -2*y*yc + yc² − R² = 0
+        // y² + y(−2*yc) + (a² - 2*a*xc + xc² + yc² − R²) = 0
+        // On a donc du second degré de la forme ax² + bx + c = 0 avec:
+        ArrayList<Point> points_intersection = new ArrayList<>();
+        double A = 1;
+        double B = -2*yc;
+        double C = a*a - 2*a*xc + xc*xc + yc*yc - r*r;
+        double delta = B*B - 4*A*C;
+
+        if (delta > 0) {
+            double x = a;
+            double y = (-B - Math.sqrt(delta)) / (2*A);
+            points_intersection.add(new Point(x, y));
+
+            x = a;
+            y = (-B + Math.sqrt(delta)) / (2*A);
+            points_intersection.add(new Point(x, y));
+        }
+        else if (delta == 0) {
+            double y = -B / (2*A);
+            points_intersection.add(new Point(a, y));
+        }
+        return points_intersection;
+    }
+
+
 }
--- player/src/test/java/fr/unice/polytech/si3/qgl/ajil/maths/CalculIntersectionTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/ajil/maths/CalculIntersectionTest.java
@@ -2,9 +2,12 @@ package fr.unice.polytech.si3.qgl.ajil.maths;
 
 import fr.unice.polytech.si3.qgl.ajil.Position;
 import fr.unice.polytech.si3.qgl.ajil.shape.Circle;
+import fr.unice.polytech.si3.qgl.ajil.shape.Point;
 import fr.unice.polytech.si3.qgl.ajil.shape.Rectangle;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class CalculIntersectionTest {
 
@@ -30,5 +33,73 @@ public class CalculIntersectionTest {
         Assertions.assertTrue(CalculIntersection.intersection(circle1,position1,new Rectangle("rectangle", 2,2,0),new Position(0,3,0))); //1 intersection
         Assertions.assertFalse(CalculIntersection.intersection(circle1,position1,rectangle,new Position(2,0,0))); // 0 intersection
         Assertions.assertTrue(CalculIntersection.intersection(circle1,position1,new Rectangle("rectangle", 1.5,1.5,0),new Position(-2,1,Math.PI/4))); // cercle l'un dans l'autre
+    }
+
+    //Nouvelle version
+
+    @Test
+    void intersectionPointCircleCircle() {
+        Circle circle = new Circle("circle", 2);
+        Position position1 = new Position(0,2,0);
+        Circle circle2 = new Circle("circle",3);
+        Position position2 = new Position(4,1,0);
+        System.out.println(CalculIntersection.intersectionPointCircleCircle(circle, position1, circle2, position2));
+    }
+
+    @Test
+    void intersectionPointCircleDroite(){
+        Circle circle = new Circle("circle", 2);
+        Position position1 = new Position(0,2,0);
+        Point point1 = new Point(4.2, 0.68);
+        Point point2 = new Point(-3.46, 5.14);
+        List<Point> result = CalculIntersection.intersectionPointDroiteCircle(circle, position1, point1, point2);
+        Assertions.assertEquals( -1.02, result.get(0).getX(), 0.1);
+        Assertions.assertEquals( 3.72, result.get(0).getY(), 0.1);
+        Assertions.assertEquals( 2, result.get(1).getX(), 0.1);
+        Assertions.assertEquals( 1.96, result.get(1).getY(), 0.1);
+
+        //vertical
+
+        position1 = new Position(0,2,0);
+        point1 = new Point(-1, 0);
+        point2 = new Point(-1, 5);
+        result = CalculIntersection.intersectionPointDroiteCircle(circle, position1, point1, point2);
+        Assertions.assertEquals( -1, result.get(0).getX(), 0.1);
+        Assertions.assertEquals( 0.27, result.get(0).getY(), 0.1);
+        Assertions.assertEquals( -1, result.get(1).getX(), 0.1);
+        Assertions.assertEquals( 3.73, result.get(1).getY(), 0.1);
+
+    }
+    @Test
+    void intersectionPointSegmentDroite() {
+        Circle circle = new Circle("circle", 2);
+        Position position1 = new Position(0, 2, 0);
+        Point point1 = new Point(0.56, 2.94);
+        Point point2 = new Point(3.52, -0.8);
+        List<Point> result = CalculIntersection.intersectionCircleSegment(circle, position1, point1, point2);
+        Assertions.assertEquals( 1.87, result.get(0).getX(), 0.01);
+        Assertions.assertEquals( 1.28, result.get(0).getY(), 0.01);
+
+        point1 = new Point(2, 4);
+        point2 = new Point(2, -1);
+        result = CalculIntersection.intersectionPointDroiteCircle(circle, position1, point1, point2);
+        Assertions.assertEquals( 2, result.get(0).getX(), 0.1);
+        Assertions.assertEquals( 2, result.get(0).getY(), 0.1);
+    }
+
+    @Test
+    void intersectionPointCircleRectangleTest(){
+        Circle circle1 = new Circle("circle",2);
+        Position position1 = new Position(0,2,0.680678);
+        Rectangle rectangle = new Rectangle("rectangle",2.27,4.86,2.25);
+        Position position2 = new Position(2.03,1.58,0);
+        List<Point> result = CalculIntersection.intersectionPointCircleRectangle(circle1,position1, rectangle, position2);
+        Assertions.assertEquals(1.39, result.get(0).getX(), 0.01);
+        Assertions.assertEquals(0.56, result.get(0).getY(), 0.01);
+        Assertions.assertEquals(0.9, result.get(2).getX(), 0.01);
+        Assertions.assertEquals(3.79, result.get(2).getY(), 0.01);
+
+
+
     }
 }
