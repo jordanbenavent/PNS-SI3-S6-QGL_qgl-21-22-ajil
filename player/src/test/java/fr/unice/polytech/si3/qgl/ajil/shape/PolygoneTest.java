8a1ca8deba27adf4b2b50281c48b1d8177a00f4c
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2022-05-20 13:09:18 +0200

    Elimination des mutants dans Shapes #92

--- player/src/test/java/fr/unice/polytech/si3/qgl/ajil/shape/PolygoneTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/ajil/shape/PolygoneTest.java
@@ -13,8 +13,12 @@ class PolygoneTest {
         Polygone poly1 = new Polygone("polygon", 0, new Point[0]);
         poly1.setVertices(points);
         Polygone poly2 = new Polygone("polygon", 0, points);
+        Polygone poly3 = new Polygone("polygon", 3, points);
+        Circle circle1 = new Circle("circle", 1);
         Assertions.assertTrue(poly1.equals(poly2));
         Assertions.assertEquals(-33521758, poly1.hashCode());
         Assertions.assertEquals(-33521758, poly2.hashCode());
+        Assertions.assertFalse(poly1.equals(poly3));
+        Assertions.assertFalse(poly1.equals(circle1));
     }
 }
--- player/src/test/java/fr/unice/polytech/si3/qgl/ajil/shape/RectangleTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/ajil/shape/RectangleTest.java
@@ -11,7 +11,36 @@ class RectangleTest {
     void testEquals() {
         Rectangle rectangle1 = new Rectangle("rectangle", 100,100,0);
         Rectangle rectangle2 = new Rectangle("rectangle", 100,100,0);
+        Rectangle rectangle3 = new Rectangle("rectangle", 100,100,3);
+        Circle circle3 = new Circle("circle", 3);
         Assertions.assertTrue(rectangle1.equals(rectangle2));
         Assertions.assertEquals(rectangle1.hashCode(), rectangle2.hashCode());
+        Assertions.assertNotEquals(0, rectangle1.hashCode());
+        Assertions.assertFalse(rectangle1.equals(rectangle3));
+        Assertions.assertFalse(rectangle1.equals(circle3));
+    }
+
+    @Test
+    void testEqualsCircle() {
+        Circle circle1 = new Circle("circle", 1);
+        Circle circle2 = new Circle("circle", 1);
+        Circle circle3 = new Circle("circle", 3);
+        Rectangle rectangle2 = new Rectangle("rectangle", 100,100,0);
+        Assertions.assertEquals(circle1.hashCode(), circle2.hashCode());
+        Assertions.assertNotEquals(0, circle1.hashCode());
+        Assertions.assertFalse(circle1.equals(rectangle2));
+        Assertions.assertTrue(circle1.equals(circle2));
+        Assertions.assertFalse(circle1.equals(circle3));
+    }
+
+    @Test
+    void testEqualsPoint() {
+        Point point1 = new Point(1,1);
+        Point point2 = new Point(1,1);
+        Point point3 = new Point(3,3);
+        Circle circle3 = new Circle("circle", 3);
+        Assertions.assertTrue(point1.equals(point2));
+        Assertions.assertFalse(point1.equals(point3));
+        Assertions.assertFalse(point1.equals(circle3));
     }
 }
